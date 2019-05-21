962c8aa70168f6c15c31eacff28da3ed15c265e5
M. Fauzilkamil <jerungkun@gmail.com>
2019-05-22 03:02:03 +0800

    beginning my htmlcss notes repo

---
+++ README.md
@@ -0,0 +1,6 @@
+## My Notes About HTML CSS
+
+This is the place where I'll be writing about my HTML and CSS exercise.
+
+It's been a long time I didn't practice writing plain HTML and CSS. So, I made this repo to keep my files and notes about it.
+
---
+++ cssgrid/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>This is my php</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+this is from my php
+</body>
+</html>
